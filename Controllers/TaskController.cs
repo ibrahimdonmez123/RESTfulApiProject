fd321bc1c018d4f3e4a1ec98c09a634915189102
ibrahimdonmez123 <donmez2k@gmail.com>
2024-01-31 00:46:23 +0300

    Add project files.

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,46 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[ApiController]
+[Route("api/tasks")]
+public class TaskController : ControllerBase
+{
+    private static List<TaskModel> tasks = new List<TaskModel>
+    {
+        new TaskModel { Id = 1, Task = "Yazılım calis", Done = false },
+        new TaskModel { Id = 2, Task = "Çöpü çıkart", Done = false }
+    };
+
+    [HttpGet]
+    public ActionResult<IEnumerable<TaskModel>> GetTasks()
+    {
+        return Ok(new { tasks });
+    }
+
+    [HttpGet("{taskId}")]
+    public ActionResult<TaskModel> GetTask(int taskId)
+    {
+        var task = tasks.Find(t => t.Id == taskId);
+        if (task == null)
+        {
+            return NotFound(new { error = "Görev bulunamadı" });
+        }
+        return Ok(new { task });
+    }
+
+    [HttpPost]
+    public ActionResult<TaskModel> CreateTask([FromBody] TaskModel newTask)
+    {
+        if (newTask == null || string.IsNullOrEmpty(newTask.Task))
+        {
+            return BadRequest(new { error = "Görev eklenmeli" });
+        }
+
+        newTask.Id = tasks.Count + 1;
+        newTask.Done = false;
+        tasks.Add(newTask);
+
+        return CreatedAtAction(nameof(GetTask), new { taskId = newTask.Id }, new { task = newTask });
+    }
+}
---
+++ TaskModel.cs
@@ -0,0 +1,7 @@
+﻿
+public class TaskModel
+{
+    public int Id { get; set; }
+    public string Task { get; set; }
+    public bool Done { get; set; }
+}
